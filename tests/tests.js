02e530be0af96b244eb8032442a7b487e17293b4
lebbe <lebchen@gmail.com>
2014-03-31 11:56:43 +0200

    adding tests

---
+++ tests/tests.js
@@ -0,0 +1,20 @@
+module('memento.js tests');
+test('Test a simple form', function() {
+	var form =
+		'<form>' +
+			'<input type="text" name="test1">' +
+			'<input type="text" name="test2">' +
+			'<input type="text" name="test3">' +
+			'<input type="text" name="test4">' +
+		'</form>';
+	var $form1 = $(form);
+
+	$form1.memento();
+
+	$form1.find('[name=test1]').prop('value', 'Test 1').trigger('change');;
+
+	var $form2 = $(form);
+	$form2.memento();
+
+	ok($form1.find('[name=test1]').prop('value') == $form2.find('[name=test1]').prop('value'), 'Passed!');
+});
