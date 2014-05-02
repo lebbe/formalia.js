a9bb68df70dbb7d2414ba77fece95c458ba414b4
lebbe <lebchen@gmail.com>
2014-05-02 14:47:48 +0200

    Should have the feature as well

---
+++ formalia.js
@@ -0,0 +1,96 @@
+/**
+ * formalia.js copyright Lars-Erik Bruce 2014.
+ *
+ * See license file for license.
+ */
+(function($, g) {
+
+if (!localStorage in g) {
+	console.log('Could not find localStorage.');
+	return;
+}
+
+
+$.fn.formalia = function(actionOrOptions) {
+
+	function restoreForm(form, o) {
+		var UI = o.UIC(form);
+		var cache = JSON.parse(localStorage.getItem(UI));
+		if(!cache || typeof cache !== 'object') return;
+		for(field in cache) {
+			$(form).find('[name=' + field + ']').prop('value', cache[field]);
+		}
+		$(form).trigger('formRestore', {fieldsRestored: cache});
+	}
+
+	/**
+	 *
+	 */
+	function monitorForm(form, o) {
+		var UI = o.UIC(form);
+		var cache = JSON.parse(localStorage.getItem(UI));
+		if(!cache || typeof cache !== 'object') cache = {};
+		$(form).on('change', function(e) {
+			var $t = $(e.target),
+				n = $t.prop('name'),
+				v = $t.prop('value');
+			cache[n] = v;
+			localStorage.setItem(UI, JSON.stringify(cache));
+		})
+		/**
+		 * When content in form actually is submitted, we no longer
+		 * need to remember/cache the user input.
+		 */
+		.on(o.submitEvent, function(e) {
+			localStorage.removeItem(UI);
+		});
+	}
+
+	function monitorAjax(element, options) {
+		debugger
+	}
+
+	if(typeof actionOrOptions === 'string') {
+		debugger
+		switch (actionOrOptions) {
+			case 'reset':
+			if(this[0].tagName === 'FORM')
+				this.trigger("reset");
+			else
+				this.find('form').each(function() {
+					$(this).trigger("reset");
+				});
+			break;
+		}
+		return;
+	}
+
+	var options = {
+		/** Unique Identity Creator **/
+		UIC: function UIC(form) {
+			return $(form).attr('id') + '_' + $(form).attr('name');
+		},
+		/* You can add your own event to listen on for submit. For instance,
+			you can trigger an event when the server responds with "Everything OK"
+			after a submit, if something went wrong you still want to cache the
+			user input. */
+		submitEvent: 'submit'
+	};
+	if(typeof actionOrOptions === 'object')
+		$.extend(options, actionOrOptions);
+
+	this.each(function() {
+		switch(this.tagName) {
+			case 'FORM':
+				restoreForm(this, options);
+				monitorForm(this, options);
+			break;
+			default:
+				monitorAjax(this, options);
+			break;
+		}
+	});
+	return this;
+};
+ 
+}(jQuery, window));
